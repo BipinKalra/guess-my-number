6164d86ab80dd3707e085094722597420e813b4f
BipinKalra <bipinkalra@gmail.com>
2021-01-15 17:10:39 +0530

    trying out DOM

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 "use strict";
 
+document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 13;
+
+console.log(document.querySelector(".guess").value)
