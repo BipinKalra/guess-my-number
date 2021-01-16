1e811e944a9777b49e73c07d75b1f017a9f5e673
BipinKalra <bipinkalra@gmail.com>
2021-01-16 17:15:25 +0530

    basic game logic

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 "use strict";
 
-document.querySelector(".message").textContent = "Correct Number!";
+// document.querySelector(".message").textContent = "Correct Number!";
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 13;
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 13;
 
-console.log(document.querySelector(".guess").value)
+// console.log(document.querySelector(".guess").value)
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // No guess
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+
+    document.querySelector("body").style.backgroundColor = "#60B347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // Guess too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    // Guess too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
