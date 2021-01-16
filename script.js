5c56c1c8fc1c56c166c18fa50c5e116c99be2ae1
BipinKalra <bipinkalra@gmail.com>
2021-01-16 18:13:51 +0530

    reset logic

--- script.js
+++ script.js
@@ -7,9 +7,24 @@
 
 // console.log(document.querySelector(".guess").value)
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+// document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".again").addEventListener("click", function () {
+  // Resetting everything by reloading the page
+  // location.reload();
+
+  // Without reloading the page
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -21,6 +36,7 @@ document.querySelector(".check").addEventListener("click", function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60B347";
     document.querySelector(".number").style.width = "30rem";
